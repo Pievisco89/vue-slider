bf65d79fa548827addd3a6468394970a77fa38a3
Pievisco89 <pie-89@live.it>
2021-05-05 16:11:25 +0200

    autoplay

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+
+  el:'#app',
+  data:{
+    counterImg: 0,
+    images: [
+      'assets/img/().jpg',
+      'assets/img/agaetis-byrjun.jpg',
+      'assets/img/kveikur.jpg',
+      'assets/img/takk.jpg'
+    ],
+  },
+  methods:{
+    prevImg(){
+      this.counterImg--;
+      if(this.counterImg < 0) this.counterImg = this.images.length - 1;
+    },
+    nextImg(){
+      this.counterImg++;
+      if(this.counterImg === this.images.length) this.counterImg = 0;
+    },
+    autoPlay(){
+      setInterval((this.nextImg), 1000)
+    }
+    
+  }
+
+});
